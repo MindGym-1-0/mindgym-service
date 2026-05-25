33b5758bfa36dcf62d90e20c8e93cac4494c478e
Muhammad Mustafa <127570514+MuhammadAlMashayekh@users.noreply.github.com>
2026-05-25 11:44:25 -0700

    Lint fixing

--- src/types/weekly_mission.py
+++ src/types/weekly_mission.py
@@ -1,6 +1,6 @@
-﻿from pydantic import BaseModel, Field
 from datetime import date, datetime
-from typing import List, Optional
+
+from pydantic import BaseModel, Field
 
 
 class WeeklyMissionGenerateResponse(BaseModel):
