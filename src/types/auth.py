09029eee664b7a9c65e1e3d34f3bbcaa90d84c12
Anastasiia <a.tchekanina@yandex.ru>
2026-05-28 16:04:48 -0700

    CI checks

--- src/types/auth.py
+++ src/types/auth.py
@@ -40,3 +40,4 @@ class LogoutResponse(BaseModel):
 class SignupRequest(EmailPasswordRequest):
     first_name: str = Field(min_length=1, max_length=100)
     last_name: str = Field(min_length=1, max_length=100)
+    
